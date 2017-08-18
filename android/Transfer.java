27fc8d8f9e95ae34748dfccafff951abbf37706e
Acathur <chiahungzhang@gmail.com>
2017-08-18 19:15:26 +0800

    0.1.4 released

--- android/Transfer.java
+++ android/Transfer.java
@@ -1,15 +1,15 @@
 package com.nat.weex;
 
-import com.nat.transfer.ModuleResultListener;
 import com.nat.transfer.TransferModule;
+import com.nat.transfer.ModuleResultListener;
 
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
 import com.taobao.weex.common.WXModule;
 
 /**
- * Created by Daniel on 17/2/17.
- * Copyright (c) 2017 Nat. All rights reserved.
+ * Created by Acathur on 17/2/17.
+ * Copyright (c) 2017 Instapp. All rights reserved.
  */
 
 public class Transfer extends WXModule {
@@ -19,7 +19,7 @@ public class Transfer extends WXModule {
         TransferModule.getInstance().upload(str, new ModuleResultListener() {
             @Override
             public void onResult(Object o) {
-                jsCallback.invoke(o);
+                jsCallback.invokeAndKeepAlive(o);
             }
         });
     }
